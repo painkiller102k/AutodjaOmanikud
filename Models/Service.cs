86e057e204025f945bb9738d3e3a03dd6593a0de
Martin Rossakov <103848033+painkiller102k@users.noreply.github.com>
2025-12-03 18:05:53 +0200

    datetime ja carId foreign key

--- Models/Service.cs
+++ Models/Service.cs
@@ -1,13 +1,14 @@
-﻿namespace AutodjaOmanikud
+namespace AutodjaOmanikud
 {
     public class Service
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public decimal Price { get; set; }
-        public DateTime Time { get; set; }
 
-        public int CarId { get; set; }
+        public int CarId { get; set; }  
         public Car Car { get; set; }
+
+        public DateTime Time { get; set; } 
     }
 }
