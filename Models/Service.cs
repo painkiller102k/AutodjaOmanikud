4713f7395838b8d73468158c4ef9c6040c845138
Martin Rossakov <103848033+painkiller102k@users.noreply.github.com>
2025-12-09 16:23:36 +0200

    service class update

--- Models/Service.cs
+++ Models/Service.cs
@@ -3,12 +3,13 @@ namespace AutodjaOmanikud
     public class Service
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public decimal Price { get; set; }
-
-        public int CarId { get; set; }  
-        public Car Car { get; set; }
-
+        public int CarId { get; set; }
+        public int ServiceTypeId { get; set; }
         public DateTime Time { get; set; } 
+
+        public Car Car { get; set; }
+        public ServiceType ServiceType { get; set; }
     }
+
+
 }
