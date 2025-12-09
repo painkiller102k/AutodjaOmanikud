3da47cefda7ce9573d0522641a5c4311b3b5f142
Martin Rossakov <103848033+painkiller102k@users.noreply.github.com>
2025-12-09 16:25:09 +0200

    DbContext foreign keys uuendamine service ja servicetype !

--- Data/AutoDbContext.cs
+++ Data/AutoDbContext.cs
@@ -1,7 +1,4 @@
-using AutodjaOmanikud;
 using Microsoft.EntityFrameworkCore;
-using System.Collections.Generic;
-using System.Reflection.Emit;
 
 namespace AutodjaOmanikud
 {
@@ -10,22 +7,40 @@ namespace AutodjaOmanikud
         public DbSet<Owner> Owners { get; set; }
         public DbSet<Car> Cars { get; set; }
         public DbSet<Service> Services { get; set; }
+        public DbSet<ServiceType> ServiceTypes { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             if (!optionsBuilder.IsConfigured)
             {
-                optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=AutoDatabase;Trusted_Connection=True;");
+                optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=AutoDb;Trusted_Connection=True;");
             }
         }
 
-
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-
             modelBuilder.Entity<Owner>().ToTable("Owners");
             modelBuilder.Entity<Car>().ToTable("Cars");
             modelBuilder.Entity<Service>().ToTable("Services");
+            modelBuilder.Entity<ServiceType>().ToTable("ServiceTypes");
+
+            modelBuilder.Entity<Car>()
+                .HasOne(c => c.Owner)
+                .WithMany(o => o.Cars)
+                .HasForeignKey(c => c.OwnerId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<Service>()
+                .HasOne(s => s.Car)
+                .WithMany()
+                .HasForeignKey(s => s.CarId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<Service>()
+                .HasOne(s => s.ServiceType)
+                .WithMany()
+                .HasForeignKey(s => s.ServiceTypeId)
+                .OnDelete(DeleteBehavior.Restrict);
         }
     }
 }
