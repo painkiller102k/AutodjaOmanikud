7f69ff4e1631d5e9a085312f0c1440f224e1a83c
Martin Rossakov <103848033+painkiller102k@users.noreply.github.com>
2025-12-09 16:24:08 +0200

    uue class servicetype

---
+++ Models/ServiceType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutodjaOmanikud
+{
+    public class ServiceType
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+
+        public List<Service> Services { get; set; }
+    }
+
+}
+
