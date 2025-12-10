9de267caf0e3af0ccc282d9f166d1f7439fae04c
Martin Rossakov <103848033+painkiller102k@users.noreply.github.com>
2025-12-10 17:19:06 +0200

    uuenda class service ( is paid )

--- Models/Service.cs
+++ Models/Service.cs
@@ -4,12 +4,15 @@ namespace AutodjaOmanikud
     {
         public int Id { get; set; }
         public int CarId { get; set; }
-        public int ServiceTypeId { get; set; }
-        public DateTime Time { get; set; } 
-
         public Car Car { get; set; }
+        public int ServiceTypeId { get; set; }
         public ServiceType ServiceType { get; set; }
+
+        public DateTime Time { get; set; }
+
+        public bool IsPaid { get; set; } = false;
     }
+
 
 
 }
